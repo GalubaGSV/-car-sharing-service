5059dbbfe9d0db57bf12da5ac4b6620c0b5205e9
Denys Dominskyi <dominskyj@gmail.com>
2023-05-30 00:37:24 +0300

    1. added changelog 2. fixed some settings

--- src/main/java/com/example/carsharingservice/model/User.java
+++ src/main/java/com/example/carsharingservice/model/User.java
@@ -1,6 +1,7 @@
 package com.example.carsharingservice.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +12,7 @@ import lombok.Data;
 
 @Data
 @Table(name = "users")
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
