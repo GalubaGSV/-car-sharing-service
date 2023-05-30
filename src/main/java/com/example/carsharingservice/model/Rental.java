3ce10a4ffe7239983951f28f02f15cb33dae5952
Oleksandr Lutsenko <sashagrek03@gmail.com>
2023-05-30 12:30:34 +0300

    [API] fix the imports in Rental entity

--- src/main/java/com/example/carsharingservice/model/Rental.java
+++ src/main/java/com/example/carsharingservice/model/Rental.java
@@ -1,6 +1,14 @@
 package com.example.carsharingservice.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.FetchType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
