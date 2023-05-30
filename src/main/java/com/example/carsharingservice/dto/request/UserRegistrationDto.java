c5aadce32507dd6dca37a4fccd524c1d3d98d648
Denys Dominskyi <dominskyj@gmail.com>
2023-05-30 18:29:20 +0300

    [API] add AuthenticationController add AuthenticationException add AuthenticationService create annotations @FieldsValueMatch and @ValidEmail add UserLoginDto add UserRegistrationDto

---
+++ src/main/java/com/example/carsharingservice/dto/request/UserRegistrationDto.java
@@ -0,0 +1,25 @@
+package com.example.carsharingservice.dto.request;
+
+import com.example.carsharingservice.lib.FieldsValueMatch;
+import com.example.carsharingservice.lib.ValidEmail;
+import jakarta.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@FieldsValueMatch(field = "password", fieldMatch = "repeatPassword")
+public class UserRegistrationDto {
+    @ValidEmail
+    private String email;
+    @NotEmpty(message = "The password couldn't be empty")
+    @Size(min = 8, message = "Password must be at least 8 symbols long")
+    private String password;
+    @NotEmpty(message = "The password couldn't be empty")
+    @Size(min = 8, message = "Password must be at least 8 symbols long")
+    private String repeatPassword;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+}
