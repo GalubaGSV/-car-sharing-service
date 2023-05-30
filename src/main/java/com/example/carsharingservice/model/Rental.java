50be0ff6a36e95b526102c8fd15c42a58d162c3a
Oleksandr Lutsenko <sashagrek03@gmail.com>
2023-05-30 12:26:48 +0300

    [API] add RentalRepository, RentalService and its implementation, change the type of fields in Rental entity from Date to LocalDateTime

--- src/main/java/com/example/carsharingservice/model/Rental.java
+++ src/main/java/com/example/carsharingservice/model/Rental.java
@@ -1,14 +1,6 @@
 package com.example.carsharingservice.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
