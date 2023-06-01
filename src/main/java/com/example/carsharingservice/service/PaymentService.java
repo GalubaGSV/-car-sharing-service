7d86058ce0f73c222b1b9ac092cadbfa29b80450
Sergey Golubchenko <121030403+GalubaGSV@users.noreply.github.com>
2023-06-01 09:19:13 +0300

    [STRIPE]: add calculation price methods (#24)
    * [API]: add PaymentMapper, PaymentRequestDto, PaymentResponseDto
    
    * [API]: add methods implementation to PaymentController
    
    * [STRIPE]: add StripePaymentService. Implement createPaymentSession method
    
    * [STRIPE]: add calculation price methods

--- src/main/java/com/example/carsharingservice/service/PaymentService.java
+++ src/main/java/com/example/carsharingservice/service/PaymentService.java
@@ -1,10 +1,13 @@
 package com.example.carsharingservice.service;
 
 import com.example.carsharingservice.model.Payment;
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface PaymentService {
     Payment add(Payment payment);
 
     List<Payment> findByRentalUserId(Long userId);
+
+    BigDecimal calculatePrice(Payment payment);
 }
--- src/main/java/com/example/carsharingservice/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/example/carsharingservice/service/impl/PaymentServiceImpl.java
@@ -1,16 +1,23 @@
 package com.example.carsharingservice.service.impl;
 
 import com.example.carsharingservice.model.Payment;
+import com.example.carsharingservice.model.PaymentType;
+import com.example.carsharingservice.model.Rental;
 import com.example.carsharingservice.repository.PaymentRepository;
 import com.example.carsharingservice.service.PaymentService;
+import java.math.BigDecimal;
+import java.time.Duration;
 import java.util.List;
+import com.example.carsharingservice.service.RentalService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
 public class PaymentServiceImpl implements PaymentService {
+    private static final BigDecimal FINE_MULTIPLIER = BigDecimal.valueOf(1.5);
     private final PaymentRepository paymentRepository;
+    private final RentalService rentalService;
 
     @Override
     public Payment add(Payment payment) {
@@ -20,5 +27,22 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public List<Payment> findByRentalUserId(Long userId) {
         return paymentRepository.findByRental_User_Id(userId);
+    }
+
+    @Override
+    public BigDecimal calculatePrice(Payment payment) {
+        if (payment.getPaymentType().equals(PaymentType.PAYMENT)) {
+            Rental rental = rentalService.get(payment.getRental().getId());
+            payment.setRental(rental);
+            long days = Duration.between(rental.getRentalDate(),
+                    rental.getReturnDate()).toDays();
+            return rental.getCar().getDailyFee().multiply(BigDecimal.valueOf(days));
+        }
+        Rental rental = rentalService.get(payment.getRental().getId());
+        payment.setRental(rental);
+        long days = Duration.between(rental.getReturnDate(),
+                rental.getActualReturnDate()).toDays();
+        BigDecimal fineAmount = rental.getCar().getDailyFee().multiply(FINE_MULTIPLIER);
+        return BigDecimal.valueOf(days).multiply(fineAmount);
     }
 }
