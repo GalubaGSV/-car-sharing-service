ea534b62db0bceccb6d0dc593e3300f6e5f832e4
Sergey Golubchenko <121030403+GalubaGSV@users.noreply.github.com>
2023-06-16 14:14:01 +0300

    [LIQUIBASE]: insert sample data (#42)
    [LIQUIBASE]: insert sample data

--- src/main/java/com/example/carsharingservice/controller/PaymentController.java
+++ src/main/java/com/example/carsharingservice/controller/PaymentController.java
@@ -9,17 +9,16 @@ import com.example.carsharingservice.model.Rental;
 import com.example.carsharingservice.model.Role;
 import com.example.carsharingservice.model.User;
 import com.example.carsharingservice.service.PaymentService;
-import com.example.carsharingservice.service.RentalService;
 import com.example.carsharingservice.service.StripePaymentService;
 import com.example.carsharingservice.service.UserService;
 import com.example.carsharingservice.service.impl.TelegramNotificationService;
 import com.example.carsharingservice.service.mapper.DtoMapper;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -35,22 +34,26 @@ import org.springframework.web.servlet.view.RedirectView;
 
 @RequiredArgsConstructor
 @RestController
-@SecurityRequirement(name = "Bearer Authentication")
-@Tag(name = "Payment", description = "The Payment API. "
-        + "Contains all the operations that can be performed on a customer/manager.")
 @RequestMapping("/payments")
 public class PaymentController {
     private final DtoMapper<PaymentRequestDto, PaymentResponseDto, Payment> mapper;
     private final PaymentService paymentService;
     private final StripePaymentService stripePaymentService;
     private final UserService userService;
-    private final RentalService rentalService;
     private final TelegramNotificationService telegramNotificationService;
 
     @Operation(summary = "Get payment by user id ", description = "Get payment by user id ")
     @GetMapping
-    public List<PaymentResponseDto> getUserPayments(@RequestParam(name = "user_id") Long userId,
-            Authentication auth, @RequestParam(defaultValue = "20") Integer count,
+    public List<PaymentResponseDto> getUserPayments(
+            @Parameter(description = "User id",
+            schema = @Schema(type = "integer", defaultValue = "1"))
+            @RequestParam(name = "user_id") Long userId,
+            Authentication auth,
+            @Parameter(description = "Payment per page",
+            schema = @Schema(type = "integer"))
+            @RequestParam(defaultValue = "20") Integer count,
+            @Parameter(description = "Page number",
+            schema = @Schema(type = "integer"))
             @RequestParam(defaultValue = "0") Integer page) {
         Pageable pageRequest = PageRequest.of(page, count);
         UserDetails details = (UserDetails) auth.getPrincipal();
@@ -73,7 +76,13 @@ public class PaymentController {
 
     @Operation(summary = "Create payment", description = "Create payment")
     @PostMapping
-    public PaymentResponseDto addPayment(@RequestBody StripeUserRequestDto stripeUserRequestDto) {
+    public PaymentResponseDto addPayment(
+            @Parameter(schema = @Schema(type = "String",
+                    defaultValue = "{\n"
+                            + "    \"rentalId\":1,\n"
+                            + "    \"paymentType\":\"Payment\"    \n"
+                            + "}"))
+            @RequestBody StripeUserRequestDto stripeUserRequestDto) {
         Payment payment = new Payment();
         payment.setPaymentType(PaymentType.PAYMENT);
         Rental rental = new Rental();
@@ -86,7 +95,7 @@ public class PaymentController {
         telegramNotificationService.sendMessage(String
                 .format("New payment was created. \n"
                         + "Payment info: %s \n", payment
-                ), rentalService.get(stripeUserRequestDto.getRentalId()).getUser());
+                ));
 
         return mapper.mapToDto(paymentService.add(payment));
     }
--- src/main/java/com/example/carsharingservice/controller/RentalController.java
+++ src/main/java/com/example/carsharingservice/controller/RentalController.java
@@ -11,10 +11,10 @@ import com.example.carsharingservice.service.RentalService;
 import com.example.carsharingservice.service.UserService;
 import com.example.carsharingservice.service.impl.TelegramNotificationService;
 import com.example.carsharingservice.service.mapper.DtoMapper;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -29,9 +29,6 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/rentals")
-@SecurityRequirement(name = "Bearer Authentication")
-@Tag(name = "Rental", description = "The Rental API. "
-        + "Contains all the operations that can be performed on a customer/manager.")
 public class RentalController {
     private final RentalService rentalService;
     private final DtoMapper<RentalRequestDto, RentalResponseDto, Rental> rentalMapper;
@@ -42,27 +39,41 @@ public class RentalController {
 
     @Operation(summary = "Add rental", description = "Add rental")
     @PostMapping
-    public RentalResponseDto add(@RequestBody RentalRequestDto rentalRequestDto) {
+    public RentalResponseDto add(
+            @Parameter(schema = @Schema(type = "String",
+                    defaultValue = "{\n"
+                            + "    \"rentalDate\":\"2023-05-12T00:00:00.000Z\",\n"
+                            + "    \"returnDate\":\"2023-06-12T00:00:00.000Z\",    \n"
+                            + "    \"carId\":1, \n"
+                            + "    \"userId\":1\n"
+                            + "}"))
+            @RequestBody RentalRequestDto rentalRequestDto) {
         Rental createdRental = rentalService.add(rentalMapper.mapToModel(rentalRequestDto));
-        User userFromDb = userService.get(createdRental.getUser().getId());
         telegramNotificationService.sendMessage(String
                 .format(
                                 "New rental was created.\n"
-                                        + "Rental info: %s\n"
-                                        + "User info: %s\n"
-                                        + "Car info: %s", rentalMapper.mapToDto(createdRental),
-                        userMapper.mapToDto(userFromDb),
-                        carService.get(createdRental.getCar().getId())), userFromDb);
+                                        + "Rental info: \n"
+                                        + "User info: \n"
+                                        + "Car info: ", createdRental,
+                        userMapper.mapToDto(userService.get(createdRental.getUser().getId())),
+                        carService.get(createdRental.getCar().getId())));
         return rentalMapper.mapToDto(createdRental);
     }
 
-    @Operation(summary = "Get rental by user and status",
-            description = "Get rental by user and status")
+    @Operation(summary = "Get rental by user and status", description = "Get rental by user and status")
     @GetMapping
     public List<RentalResponseDto> getRentalsByUserIdAndStatus(
+            @Parameter(description = "User id",
+            schema = @Schema(type = "integer", defaultValue = "1"))
             @RequestParam(name = "user_id") Long id,
+            @Parameter(description = "Status",
+            schema = @Schema(type = "boolean", defaultValue = "true"))
             @RequestParam(name = "is_active") boolean isActive,
+            @Parameter(description = "Rental per page",
+            schema = @Schema(type = "integer"))
             @RequestParam(defaultValue = "20") Integer count,
+            @Parameter(description = "Page number",
+            schema = @Schema(type = "integer"))
             @RequestParam(defaultValue = "0") Integer page) {
         Pageable pageRequest = PageRequest.of(page, count);
         return rentalService.getRentalsByUserIdAndStatus(id, isActive, pageRequest)
@@ -73,25 +84,39 @@ public class RentalController {
 
     @Operation(summary = "Get rental by id", description = "Get rental by id")
     @GetMapping("/{id}")
-    public RentalResponseDto get(@PathVariable Long id) {
+    public RentalResponseDto get(
+            @Parameter(description = "Rental id",
+            schema = @Schema(type = "integer", defaultValue = "1"))
+            @PathVariable Long id) {
         return rentalMapper.mapToDto(rentalService.get(id));
     }
 
     @Operation(summary = "Set actual return date ", description = "Set actual return date ")
     @PostMapping("/{id}/return")
-    public RentalResponseDto returnCar(@PathVariable Long id,
+    public RentalResponseDto returnCar(
+            @Parameter(description = "Rental id",
+            schema = @Schema(type = "integer", defaultValue = "1"))
+            @PathVariable Long id,
+            @Parameter(schema = @Schema(type = "String",
+                    defaultValue = "{\n"
+                            + "  \"id\": 1,\n"
+                            + "  \"rentalDate\":\"2023-06-01T23:39:42.979Z\",\n"
+                            + "  \"returnDate\":\"2023-06-01T23:39:42.979Z\",\n"
+                            + "  \"actualReturnDate\":\"2023-06-01T23:39:42.979Z\",\n"
+                            + "  \"carId\": 1,\n"
+                            + "  \"userId\": 1\n"
+                            + "}"))
             @RequestBody RentalRequestDto rentalRequestDto) {
         Rental processedRental = rentalService
                 .returnCar(id, rentalMapper.mapToModel(rentalRequestDto));
-        User userFromDb = userService.get(processedRental.getUser().getId());
         telegramNotificationService.sendMessage(String
                 .format(
                                 "The car was returned.\n"
-                                        + "Rental info: %s\n"
-                                        + "User info: %s\n"
-                                        + "Car info: %s\n", rentalMapper.mapToDto(processedRental),
-                        userMapper.mapToDto(userFromDb),
-                        carService.get(processedRental.getCar().getId())), userFromDb);
+                                        + "Rental info: \n"
+                                        + "User info: \n"
+                                        + "Car info: \n", processedRental,
+                        userMapper.mapToDto(userService.get(processedRental.getUser().getId())),
+                        carService.get(processedRental.getCar().getId())));
         return rentalMapper.mapToDto(processedRental);
     }
 }
