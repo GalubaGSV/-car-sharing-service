f39aa439a344648d9056ee72d977264689a6f2d7
Sergiy Golubchenko <galuba1@gmail.com>
2023-06-16 17:05:04 +0300

    [LIQUIBASE]: insert sample data

--- src/main/java/com/example/carsharingservice/controller/UserController.java
+++ src/main/java/com/example/carsharingservice/controller/UserController.java
@@ -7,8 +7,8 @@ import com.example.carsharingservice.model.User;
 import com.example.carsharingservice.service.UserService;
 import com.example.carsharingservice.service.mapper.DtoMapper;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.NoSuchElementException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -24,19 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/users")
+@SecurityRequirement(name = "Bearer Authentication")
+@Tag(name = "User", description = "The User API. "
+        + "Contains all the operations that can be performed on a customer/manager.")
 public class UserController {
     private final UserService userService;
     private final DtoMapper<UserRequestDto, UserResponseDto, User> userMapper;
 
     @Operation(summary = "Update user role", description = "Update user role")
     @PutMapping("/{id}/role")
-    public UserResponseDto updateRole(
-            @Parameter(description = "User id",
-            schema = @Schema(type = "integer", defaultValue = "1"))
-            @PathVariable Long id,
-            @Parameter(description = "User role (Customer, Manager)",
-            schema = @Schema(type = "string", defaultValue = "Customer"))
-            @RequestParam String role) {
+    public UserResponseDto updateRole(@PathVariable Long id,
+                                      @RequestParam String role) {
         try {
             Role.valueOf(role);
             User user = userService.get(id);
@@ -45,7 +43,6 @@ public class UserController {
         } catch (IllegalArgumentException e) {
             throw new RuntimeException("There is no such role!", e);
         }
-
     }
 
     @Operation(summary = "Get current user info", description = "Get current user info")
@@ -60,14 +57,6 @@ public class UserController {
     @Operation(summary = "Update current user info", description = "Update current user info")
     @PutMapping("/me")
     public UserResponseDto updateInfo(Authentication auth,
-                                      @Parameter(schema = @Schema(type = "String",
-                                              defaultValue = "{\n"
-                                                      + "  \"email\": \"email@gmail.com\",\n"
-                                                      + "  \"firstName\": \"Bob\",\n"
-                                                      + "  \"lastName\": \"Bobson\",\n"
-                                                      + "  \"password\": \"12345678\",\n"
-                                                      + "  \"role\": \"MANAGER\"\n"
-                                                      + "}"))
                                       @RequestBody UserRequestDto dto) {
         UserDetails details = (UserDetails) auth.getPrincipal();
         String email = details.getUsername();
